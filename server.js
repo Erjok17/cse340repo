a87d6b022a0f1339961b99cae3236e280222d3bf
Erjok17 <mrerjok@gmail.com>
2025-03-04 09:15:58 +0300

    yeah

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const path = require("path"); // Add this to handle file paths
 
 const app = express();
 
 // View Engine and Templates
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Set the views directory
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // Path to your layout file
+
 
 // Routes
 app.get("/", (req, res) => {
